c5fa4ba74a89596754b238f045c123afb2add816
Wojciech Kozyra <wkozyra95@gmail.com>
2022-10-06 00:26:53 +0200

    fix right side + some remaps

--- keyboards/handwired/dactyl_manuform/4x5_rev2/keymaps/custom/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5_rev2/keymaps/custom/keymap.c
@@ -9,8 +9,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
                  LOWER,   KC_RALT,                                                                          KC_BSPC, LOWER, 
-                          KC_ESC,  KC_LGUI, LOWER,                                        KC_BSPC, KC_SPC,  KC_TAB,
-                          KC_LSFT, KC_LCTL, KC_RALT,                                      KC_DEL,  RAISE,   KC_ENT
+                          KC_ESC,  KC_LGUI, KC_LCTL,                                      RAISE,   KC_SPC,  KC_TAB,
+                          LOWER ,  KC_LSFT, KC_RALT,                                      KC_DEL,  KC_ENT,  KC_BSPC
     ),
     [1] = LAYOUT(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
